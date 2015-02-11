72a5e673ceebbec6867077be829e8eac61dab410
Matt <matthewmorgante@gmail.com>
2015-02-11 20:36:04 +0800

    fixed aws

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 config.paperclip_defaults = { 
-  :storage = > :s3, 
-  :s3_credentials = > { 
-    :bucket = > ENV[' AWS_BUCKET'], 
-    :access_key_id = > ENV[' AWS_ACCESS_KEY_ID'], 
-    :secret_access_key = > ENV[' AWS_SECRET_ACCESS_KEY'] 
+  :storage => :s3, 
+  :s3_credentials => { 
+    :bucket => ENV['AWS_BUCKET'], 
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'], 
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] 
     } 
     } 
 end
